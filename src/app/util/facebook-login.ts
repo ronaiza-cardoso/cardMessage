f7808d41b97d6df8902bd3293e46d341dc27a77e
Ronaiza Cardoso <ronaizacard@gmail.com>
2017-01-21 12:30:30 -0200

    taking user id

--- src/app/util/facebook-login.ts
+++ src/app/util/facebook-login.ts
@@ -3,7 +3,7 @@ import { Facebook } from 'ionic-native';
 export class FacebookLogin {
   static login (successCallback, errorCallback) {
     Facebook.login(['user_friends']).then(response => {
-      successCallback(response);
+      successCallback(response.authResponse.userID);
     }, error => {
       errorCallback(error);
     });
